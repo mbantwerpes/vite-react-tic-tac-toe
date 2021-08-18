80bcafcedba05b427a6645d40df174daa846eb40
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-18 11:24:15 +0200

    Lift state up

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 
 function Game(): JSX.Element {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i: number) {
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? 'X' : 'O';
+
+    setHistory(history.concat([{ squares: squares }]));
+    setXIsNext(!xIsNext);
+  }
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
+        <div>{status}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
@@ -16,3 +40,26 @@ function Game(): JSX.Element {
 }
 
 export default Game;
+
+function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    // if (squares[a] && (squares[a] === squares[b]) && (squares[a] === squares[c])) {
+    //   return squares[a];
+    // }
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
